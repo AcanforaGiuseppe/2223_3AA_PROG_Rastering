ef3e389a405a9d562802f838b9387b0e5912e9f6
Pacca38 <enrypacca@gmail.com>
2023-02-10 16:54:05 +0100

    Finished parser implementation

--- test/include/obj-parser.h
+++ test/include/obj-parser.h
@@ -4,10 +4,34 @@
 #include <stdlib.h>
 
 typedef struct {
+    float x;
+    float y;
+    float z;
+} obj_parser_float3_t;
+
+typedef struct {
+    float x;
+    float y;
+} obj_parser_float2_t;
+
+typedef struct {
+    obj_parser_float3_t position;
+    obj_parser_float2_t uv;
+    obj_parser_float3_t normal;
+} obj_vertex_t;
+
+typedef struct {
+    obj_vertex_t v1;
+    obj_vertex_t v2;
+    obj_vertex_t v3;
+} obj_triangle_t;
+
+typedef struct {
     size_t vertex_count;
     size_t vertex_texture_count;
     size_t vertex_normal_count;
     size_t face_count;
+    obj_triangle_t* triangles;
 } obj_t;
 
 obj_t* obj_parser_parse(const char* path);
@@ -49,8 +73,115 @@ obj_t* obj_parser_parse(const char* path) {
             mesh->face_count++;
         }
     }
+    rewind(f);
 
+    size_t v_index = 0;
+    obj_parser_float3_t* vertexes = (obj_parser_float3_t*)malloc(sizeof(obj_parser_float3_t) * mesh->vertex_count);
+    size_t vt_index = 0;
+    obj_parser_float2_t* vertexes_texture = (obj_parser_float2_t*)malloc(sizeof(obj_parser_float2_t) * mesh->vertex_texture_count);
+    size_t vn_index = 0;
+    obj_parser_float3_t* vertexes_normal = (obj_parser_float3_t*)malloc(sizeof(obj_parser_float3_t) * mesh->vertex_normal_count);
+    size_t f_index = 0;
+    int* faces = (int*)malloc(sizeof(int) * 9 * mesh->face_count);
+
+    while(fgets(buffer, buffer_size, f)) {
+        char* context;
+        char* token = strtok_s(buffer, " ", &context);
+        if (strcmp(token, "v") == 0) {
+            //x
+            token = strtok_s(NULL, " ", &context);
+            vertexes[v_index].x = atof(token);
+            //y
+            token = strtok_s(NULL, " ", &context);
+            vertexes[v_index].y = atof(token);
+            //z
+            token = strtok_s(NULL, " ", &context);
+            vertexes[v_index].z = atof(token);
+            v_index++;
+        }
+        else if (strcmp(token, "vt") == 0) {
+            //x
+            token = strtok_s(NULL, " ", &context);
+            vertexes_texture[vt_index].x = atof(token);
+            //y
+            token = strtok_s(NULL, " ", &context);
+            vertexes_texture[vt_index].y = atof(token);
+            vt_index++;
+        }
+        else if (strcmp(token, "vn") == 0) {
+            //x
+            token = strtok_s(NULL, " ", &context);
+            vertexes_normal[vn_index].x = atof(token);
+            //y
+            token = strtok_s(NULL, " ", &context);
+            vertexes_normal[vn_index].y = atof(token);
+            //z
+            token = strtok_s(NULL, " ", &context);
+            vertexes_normal[vn_index].z = atof(token);
+            vn_index++;
+        }
+        else if (strcmp(token, "f") == 0) {
+            for (size_t i = 0; i < 3; i++)
+            {
+                int index = f_index * 9 + i * 3;
+                //v
+                token = strtok_s(NULL, "/", &context);
+                faces[index + 0] = atoi(token) - 1;
+                //vt
+                token = strtok_s(NULL, "/", &context);
+                faces[index + 1] = atoi(token) - 1;
+                //vn
+                token = strtok_s(NULL, " ", &context);
+                faces[index + 2] = atoi(token) - 1;
+            }
+            f_index++;
+        }
+    }
     fclose(f);
+
+    mesh->triangles = (obj_triangle_t*)malloc(sizeof(obj_triangle_t) * mesh->face_count);
+    for (size_t i = 0; i < mesh->face_count; i++)
+    {
+        int index = i * 9;
+        //V1
+        //position
+        int vertex_index = faces[index];
+        mesh->triangles[i].v1.position = vertexes[vertex_index];
+        //uv
+        int vertex_texture_index = faces[index + 1];
+        mesh->triangles[i].v1.uv = vertexes_texture[vertex_texture_index];
+        //normal
+        int vertex_normal_index = faces[index + 2];
+        mesh->triangles[i].v1.normal = vertexes_normal[vertex_normal_index];
+
+        //V2
+        //position
+        vertex_index = faces[index + 3];
+        mesh->triangles[i].v2.position = vertexes[vertex_index];
+        //uv
+        vertex_texture_index = faces[index + 4];
+        mesh->triangles[i].v2.uv = vertexes_texture[vertex_texture_index];
+        //normal
+        vertex_normal_index = faces[index + 5];
+        mesh->triangles[i].v2.normal = vertexes_normal[vertex_normal_index];
+
+        //V3
+        //position
+        vertex_index = faces[index + 6];
+        mesh->triangles[i].v3.position =  vertexes[vertex_index];
+        //uv
+        vertex_texture_index = faces[index + 7];
+        mesh->triangles[i].v3.uv = vertexes_texture[vertex_texture_index];
+        //normal
+        vertex_normal_index = faces[index + 8];
+        mesh->triangles[i].v3.normal = vertexes_normal[vertex_normal_index];
+    }
+    
+    free(vertexes);
+    free(vertexes_normal);
+    free(vertexes_texture);
+    free(faces);
+
     return mesh;
 }
 #endif
--- test/src/obj-parser-test.c
+++ test/src/obj-parser-test.c
@@ -15,3 +15,82 @@ CLOVE_TEST(TestCountObjMeshData) {
    CLOVE_INT_EQ(1, mesh->vertex_normal_count);
    CLOVE_INT_EQ(2, mesh->face_count);
 }
+
+CLOVE_TEST(TestQuadParse) {
+   const char* base_test_path = CLOVE_EXEC_BASE_PATH();
+   const char* source_path = tut_concat_path(base_test_path, "\\resources\\quad.obj");
+
+   obj_t* mesh = obj_parser_parse(source_path);
+
+   //T1 - V1
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v1.position.x, 1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v1.position.y, -1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v1.position.z, 0.f);
+
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v1.uv.x, 1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v1.uv.y, 0.f);
+
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v1.normal.x, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v1.normal.y, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v1.normal.z, 1.f);
+
+   //T1 - V2
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v2.position.x, -1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v2.position.y, -1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v2.position.z, 0.f);
+
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v2.uv.x, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v2.uv.y, 0.f);
+
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v2.normal.x, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v2.normal.y, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v2.normal.z, 1.f);
+
+   //T1 - V3
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v3.position.x, -1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v3.position.y, 1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v3.position.z, 0.f);
+
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v3.uv.x, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v3.uv.y, 1.f);
+
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v3.normal.x, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v3.normal.y, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[0].v3.normal.z, 1.f);
+
+   //T2 - V1
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v1.position.x, 1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v1.position.y, 1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v1.position.z, 0.f);
+
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v1.uv.x, 1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v1.uv.y, 1.f);
+
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v1.normal.x, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v1.normal.y, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v1.normal.z, 1.f);
+
+   //T2 - V2
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v2.position.x, 1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v2.position.y, -1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v2.position.z, 0.f);
+
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v2.uv.x, 1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v2.uv.y, 0.f);
+
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v2.normal.x, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v2.normal.y, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v2.normal.z, 1.f);
+
+   //T2 - V3
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v3.position.x, -1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v3.position.y, 1.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v3.position.z, 0.f);
+
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v3.uv.x, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v3.uv.y, 1.f);
+
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v3.normal.x, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v3.normal.y, 0.f);
+   CLOVE_FLOAT_EQ(mesh->triangles[1].v3.normal.z, 1.f);
+}
